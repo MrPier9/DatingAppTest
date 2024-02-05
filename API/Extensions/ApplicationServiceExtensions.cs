d6989f914096c23e01a6d79c1bb6bee9f829e89b
MrPier9 <mrpier9@gmail.com>
2024-02-05 15:36:35 +0100

    like feature added

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,6 +21,7 @@ public static class ApplicationServiceExtensions
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<LogUserActivity>();
+        services.AddScoped<ILikesRepository, LikesRepository>();
 
         services.AddEndpointsApiExplorer();//service for swagger
         services.AddSwaggerGen();//service for swagger
